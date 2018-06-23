16845085f325267f759db5fcb46caabc67620299
AJ <ashleyannjorgensen@gmail.com>
2018-06-23 13:59:21 -0700

    freakin carousel

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,6 +1,6 @@
 var moviePosters = [];
 
-console.log(moviePosters);
+//console.log(moviePosters);
 var config = {
     apiKey: "AIzaSyB55nu5FoRbobBy05WCIFI5SJB1kksFaNE",
     authDomain: "project-1-44e93.firebaseapp.com",
@@ -14,23 +14,26 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 function moviePoster(index, movieFromDb) {
-
+    //var slider;
     // this is here because of the old implementation using the moviePosters array, not needed anymore with firebase update:
     if (index > 0) {
         var movie = moviePosters[index];
-        console.log(movie);
+        //console.log(movie);
         var movieURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
         $.ajax({
             url: movieURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            console.log("inside else movie poster")
+            //console.log(response);
             var genreArray = response.Genre.split(",");
             var randomGenre = genreArray[Math.floor(Math.random() * genreArray.length)];
-            var movieDiv = $("<div>");
+            var movieDiv = $("<a>").addClass("carousel-item");
             var movieImage = $("<img>");
-            console.log(genreArray);
-            console.log(randomGenre);
+            // var slider = $('.carousel');
+            // slider.carousel();
+            //console.log(genreArray);
+            //console.log(randomGenre);
             movieImage.attr(
                 {
                     "data-music": randomGenre,
@@ -39,25 +42,28 @@ function moviePoster(index, movieFromDb) {
                 }
             )
             movieDiv.append(movieImage);
-            $("#poster").append(movieDiv);
+            slider.append(movieDiv);
+            $(".carousel").append(movieDiv);
         });
+        
     }
     // this is now used with firebase:
     else {
         var movie = movieFromDb;
-        console.log(movie);
+        //console.log(movie);
         var movieURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
         $.ajax({
             url: movieURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            console.log("inside else movie poster")
+            //console.log(response);
             var genreArray = response.Genre.split(",");
             var randomGenre = genreArray[Math.floor(Math.random() * genreArray.length)];
-            var movieDiv = $("<div>");
+            var movieDiv = $("<a>").addClass("carousel-item");
             var movieImage = $("<img>");
-            console.log(genreArray);
-            console.log(randomGenre);
+            //console.log(genreArray);
+            //console.log(randomGenre);
             movieImage.attr(
                 {
                     "data-music": randomGenre,
@@ -66,19 +72,21 @@ function moviePoster(index, movieFromDb) {
                 }
             )
             movieDiv.append(movieImage);
-            $("#poster").append(movieDiv);
+            $('.carousel').append(movieDiv);
+
+            $('.carousel').carousel();
         });
     }
-
 }
+
 
 // var musicUrl = "https://accounts.spotify.com/authorize/?"
 // window.location = musicUrl + "client_id=80f8fbcd74a24b8ea1d6aab4c74ead38&response_type=token&redirect_uri=https://egretsch.github.io/project1/"
 
 $(document).on("click", ".moviePoster", function () {
-    console.log("You got music");
+    //console.log("You got music");
     $('#player').empty();
-    var token = "BQD7cRm7sZwEwwYkHSeufh_1qpgaCt1ZXwBjJSCHO7UTlku_W4qwqkF-0XVikaTAjfomZHVKk2Q3jgq0iuzIE-FmAN9ZIW1cedxn8KvGYwGU1jL49xqmNJnA0-mj_mF1z31honNGwwY";
+    var token = "BQClvjrs7FsubmLhY9vXKfLCGfagcUaCugwgQqPTiWjZyXzrs86DJ1xYLdeU6XSDjTHkJe_J9FUdG7QLj1Tc8UR6gHs6J9U90K1TY767UVWmlpbnOHkH-G1J-n8OVoc008uwyhu33Qk";
     var musicG = $(this).attr("data-music");
     var musicUrl = 'https://api.spotify.com/v1/search?q=' + musicG + '&type=playlist'
     $.ajax({
@@ -88,25 +96,25 @@ $(document).on("click", ".moviePoster", function () {
             'Authorization': 'Bearer ' + token
         },
         success: function (playlist) {
-            console.log(playlist);
+            //console.log(playlist);
             var randomMusic = playlist.playlists.items[Math.floor(Math.random() * playlist.playlists.items.length)].uri;
-            console.log(randomMusic);
+            //console.log(randomMusic);
             var musicDiv = $("<div>");
             var musicIframe = $('<iframe src="https://open.spotify.com/embed?uri=' + randomMusic + '" width="300" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media">' + '</iframe>');
             $("#player").append(musicDiv);
             musicDiv.append(musicIframe);
         },
         error: function () {
-            console.log("It failed");
+            //console.log("It failed");
         }
     });
 });
 
 
-$("#add-button").on("click", function (event) {
+$("#sudMovie").on("click", function (event) {
     event.preventDefault();
-    console.log("I'm a button");
-    var button2 = $("#button-input").val().trim();
+    //console.log("I'm a button");
+    var button2 = $("#search").val().trim();
 
     // not needed with using firebase
     moviePosters.push(button2);
@@ -123,7 +131,7 @@ $("#add-button").on("click", function (event) {
     // for (var i = 0; i < moviePosters.length; i++) {
     //     moviePoster(i);
     // }
-    $("#button-input").val(" ");
+    $("#search").val(" ");
 });
 
 
@@ -133,3 +141,4 @@ database.ref().on("child_added", function (snapShot) {
     moviePoster(-1, snapShot.val().movieFromDb);
 
 })
+//$('.carousel').carousel();
